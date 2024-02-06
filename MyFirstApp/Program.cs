8a3c1ba74066a46b6a954ef3ec80a4872f907284
Rasta <rasmus.lindau@tktk.ee>
2024-02-06 17:05:06 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//Test test?
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab kasutajat edaspidi sisestatud nimepidi
+Console.WriteLine("eneter username");
+    string userName = Console.ReadLine();
+
+Console.WriteLine("hello"+ ", " +  userName + "!");
+// string interpolation
+Console.WriteLine($"Hello, {userName} !");
